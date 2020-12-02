574a2c765cb51449cef55648487cf831b1bbb2d7
donald vallejo <donny.vallejo@gmail.com>
2020-12-01 18:15:14 -0800

    added interger and recursive

--- sorting_integer.py
+++ sorting_integer.py
@@ -1,12 +1,27 @@
+from sorting_Iterative import insertion_sort
+
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) only works when the range of potential items in the input is known ahead of time
+    TODO: Memory usage: O(n) If the rnage of potential values is big, then counting sort requires a lot of space"""
     # TODO: Find range of given numbers (minimum and maximum integer values)
+    max_value = int(max(numbers))
+    min_value = int(min(numbers))
     # TODO: Create list of counts with a slot for each number in input range
+    count_list = [0 for _ in range(max_value-min_value+ 1)]
+    output_list = [0 for _ in range(len(numbers))]
     # TODO: Loop over given numbers and increment each number's count
+    for i in range(0, len(numbers)):
+        count_list[numbers[i]- min_value] += 1
     # TODO: Loop over counts and append that many numbers into output list
+    for i in range(len(numbers)-1, -1, -1):
+        output_list[count_list[numbers[i] - min_value] - 1] = numbers
+        count_list[numbers[i] - min_value] -= 1
+    for i in range(0, len(numbers)):
+        numbers[i] = output_list[i]
+
+    return numbers
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
@@ -16,8 +31,20 @@ def bucket_sort(numbers, num_buckets=10):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    arr =  []
+    for i in range(num_buckets):
+        arr.append([])
+
+    for j in numbers:
+        index_b = int(num_buckets * j)
+        arr[index_b].append(j)
+
+    for i in range(num_buckets):
+        arr[i] = insertion_sort(arr[i])
+
+    k = 0
+    for i in range(num_buckets):
+        for j in range(len(arr[i])):
+            numbers[k] = arr[i][j]
+            k += 1
+    return numbers
--- sorting_recursive.py
+++ sorting_recursive.py
@@ -1,15 +1,9 @@
-from heapq import merge
-from sorting_iterative import insertion_sort
-import math
-
 def merge(items1, items2):
-    """Merge given lists of items, each assumed to already be in sorted order,
-    and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until one list is empty
-    # TODO: Find minimum item in both lists and append it to new list
-    # TODO: Append remaining items in non-empty list to new list
+    if items1 and items2:
+        if items1[0] > items2[0]:
+            items1, items2 = items2, items1
+        return [items1[0]] + merge(items1[1:], items2)
+    return items1 + items2
 
 
 def split_sort_merge(items):
@@ -18,68 +12,68 @@ def split_sort_merge(items):
     a list in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    items_length = len(items)
-    midpoint = math.floor(items_length)/2
     # TODO: Split items list into approximately equal halves
-    lower = []
-    higher = []
-    for  i in range(midpoint):
-        lower.append(items[i])
-        result1 = insertion_sort(lower)
-    for j in range(midpoint+1, items_length-1):
-        higher.append(items[j])
-        result2 = insertion_sort(higher)
-    return merge(result1,result2)
+    # TODO: Sort each half using any other sorting algorithm
+    # TODO: Merge sorted halves into one list in sorted order
+
 
 def merge_sort(items):
-    """Sort given items by splitting list into two approximately equal halves,
-    sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if list is so small it's already sorted (base case)
-    # TODO: Split items list into approximately equal halves
-    # TODO: Sort each half by recursively calling merge sort
-    # TODO: Merge sorted halves into one list in sorted order
-    if len(items) < 1:
+    if len(items) <= 1:
         return items
-    center = int(len(items) / 2)
+    middle = len(items) // 2
+    left = items[:middle]
+    right = items[middle:]
+    merge_sort(left)
+    merge_sort(right)
 
-    left, right = merge_sort(items[:center]), merge_sort(items[center:])
-    return merge(left, right)
+    i = j = k = 0
+    while (i < len(left)) and (j < len(right)):
+        if left[i] < right[j]:
+            items[k] = left[i]
+            i += 1
+        else:
+            items[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        items[k] = left[i]
+        i += 1
+        k+= 1
+    while j < len(right):
+        items[k] = right[j]
+        j += 1
+        k += 1
+    return items
+        
+print(merge_sort([6,8,7,1]))
+
 
 def partition(items, low, high):
-    """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
-    that range, moving pivot into index `p`, items less than pivot into range
-    `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Choose a pivot any way and document your method in docstring above
-    # TODO: Loop through all items in range [low...high]
-    # TODO: Move items less than pivot into front of range [low...p-1]
-    # TODO: Move items greater than pivot into back of range [p+1...high]
-    # TODO: Move pivot item into final position [p] and return index p
-    i = low -1
+    i = low - 1
     pivot = items[high]
     for j in range(low, high):
         if items[j] < pivot:
             i += 1
-            items[i], items[j] = items[j],items[i]
-    items[i+1], items[high] = items[high], items[i+1]
-    return (i+1)
-
+            items[i], items[j] = items[j], items[i]
+    items[i + 1], items[high] = items[high], items[i + 1]
+    return (i + 1)
+            
 
 def quick_sort(items, low=None, high=None):
-    """Sort given items in place by partitioning items in range `[low...high]`
-    around a pivot item and recursively sorting each remaining sublist range.
-    TODO: Best case running time: ??? Why and under what conditions?
-    TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if high and low range bounds have default values (not given)
-    # TODO: Check if list or range is so small it's already sorted (base case)
-    # TODO: Partition items in-place around a pivot and get index of pivot
-    # TODO: Sort each sublist range by recursively calling quick sort
-    if low < high:
-        pivot = partition(items, low, high)
-        quick_sort(items, low, pivot-1)
-        quick_sort(items, pivot+1, high)
+    length = len(items)
+    if length <= 1:
+        return items
+    else:
+        pivot = items.pop()
+    low = []
+    high = []
+    
+    for item in items:
+        if item > pivot:
+            high.append(item)
+        else:
+            low.append(item)
+    return quick_sort(low) + [pivot] + quick_sort(high)
+
+items = [2,3,6,3,7,5,8,9,7,6]    
+print(quick_sort(items, 0, 9))
